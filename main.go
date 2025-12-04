8bace7670480ad86f36361ff94f2d15e11647127
josaneluziasilva <josanesilva@gmail.com>
2025-12-04 14:26:40 -0300

    add types of values in go

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Go Essentials!")
+	fmt.Println("Hello, Go Essentials!") //String
+
+	fmt.Println(2) //Inteiros
+
+	fmt.Println(2 + 5) //Expressões
+
+	fmt.Println(true)  //Booleanos
+	fmt.Println(false) //Booleanos
 }
